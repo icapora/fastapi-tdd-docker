7240ec4e280eafeb5d0ffc6abbe6cdfc138fc162
isaias <icaporusso>
2020-08-14 01:50:26 -0300

    add newspaper and nltk to get articles with nlp

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,11 +1,11 @@
 from typing import Dict, List, Optional
 
-from app.models.pydantic import SummaryPayloadSchema
+from app.models.pydantic import SummaryPayloadSchema, SummaryUpdatePayloadSchema
 from app.models.tortoise import TextSummary
 
 
 async def post(payload: SummaryPayloadSchema) -> int:
-    summary = TextSummary(url=payload.url, summary="Dummy Dummy")
+    summary = TextSummary(url=payload.url, summary="")
     await summary.save()
     return summary.id
 
@@ -20,3 +20,20 @@ async def get(id: int) -> Optional[Dict]:
 async def get_all() -> List:
     summaries = await TextSummary.all().values()
     return summaries
+
+
+async def delete(id: int) -> int:
+    summary = await TextSummary.filter(id=id).first().delete()
+    return summary
+
+
+async def put(id: int, payload: SummaryUpdatePayloadSchema) -> Optional[Dict]:
+    summary = (
+        await TextSummary.filter(id=id)
+        .first()
+        .update(url=payload.url, summary=payload.summary)
+    )
+    if summary:
+        updated_summary = await TextSummary.filter(id=id).first().values()
+        return updated_summary[0]
+    return None
--- project/test/test_summaries.py
+++ project/test/test_summaries.py
@@ -57,3 +57,72 @@ def test_read_all_summaries(test_app_with_db):
 
     response_list = response.json()
     assert len(list(filter(lambda d: d["id"] == summary_id, response_list))) == 1
+
+
+def test_remove_summary(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://py.test"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.delete(f"/summaries/{summary_id}/")
+    assert response.status_code == 200
+    assert response.json() == {"id": summary_id, "url": "https://py.test"}
+
+
+def test_remove_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.delete("/summaries/999/")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
+
+
+def test_update_summary(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://py.test"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/",
+        data=json.dumps({"url": "https://py.test", "summary": "updated"}),
+    )
+    assert response.status_code == 200
+
+    response_dict = response.json()
+    assert response_dict["id"] == summary_id
+    assert response_dict["url"] == "https://py.test"
+    assert response_dict["summary"] == "updated"
+    assert response_dict["created_at"]
+
+
+def test_update_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.put(
+        "/summaries/999/",
+        data=json.dumps({"url": "https://py.test", "summary": "updated"}),
+    )
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
+
+
+def test_update_summary_invalid_json(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://py.test"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(f"/summaries/{summary_id}/", data=json.dumps({}))
+    assert response.status_code == 422
+    assert response.json()
+
+
+def test_update_summary_invalid_keys(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://py.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/", data=json.dumps({"url": "https://py.bar"})
+    )
+    assert response.status_code == 422
+    assert response.json()
